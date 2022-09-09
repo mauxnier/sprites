90d5e6500e78b3164f0bad1633c8c3dafdcdb904
Bazire Martin <bazire.e1905621@etud.univ-ubs.fr>
2022-09-09 09:11:06 +0200

    Ajout d'un constructeur sur la classe ZipLoader

--- util/src/main/java/fr/ensibs/util/io/ZipLoader.java
+++ util/src/main/java/fr/ensibs/util/io/ZipLoader.java
@@ -1,5 +1,48 @@
 package fr.ensibs.util.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import java.util.zip.ZipOutputStream;
+
 public class ZipLoader implements IZipLoader {
-    
+
+    JsonLoader jsonloader;
+    TextLoader textloader;
+
+    public ZipLoader(JsonLoader jsonloader, TextLoader textloader)
+    {
+        this.jsonloader = jsonloader;
+        this.textloader = textloader;
+    }
+
+    @Override
+    public Map<String, Object> load(ZipInputStream in) throws IOException, ParseException {
+        ZipInputStream zipIn = new ZipInputStream(in);
+        ZipEntry entry;
+        while ((entry = zipIn.getNextEntry()) != null) {
+            System.out.println(entry.getName());
+            readContents(zipIn);
+            zipIn.closeEntry();
+        }
+        return null;
+    }
+
+    @Override
+    public void save(Map<String, Object> resources, ZipOutputStream out) throws IOException {
+    }
+
+    private void readContents(InputStream contentsIn) throws IOException {
+        byte contents[] = new byte[4096];
+        int direct;
+        while ((direct = contentsIn.read(contents, 0, contents.length)) >= 0) {
+            System.out.println("Read " + direct + "bytes content.");
+        }
+    }
+
 }
+
+
