32009d9f02bf871a9e52867012ddab680a1b5a22
rhn <heurlin.e2105233@etud.univ-ubs.fr>
2022-09-09 09:58:39 +0200

    suppr @depreceate

--- util/src/test/java/fr/ensibs/util/io/LoadersImplTest.java
+++ util/src/test/java/fr/ensibs/util/io/LoadersImplTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * checks the integration of those classes
  *
  * @author Pascale Launay
- * @deprecated
+ *
  */
 public class LoadersImplTest
 {
