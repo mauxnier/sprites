51451ab7ecca63ef94f3691f7f0122980bf949ff
rhn <heurlin.e2105233@etud.univ-ubs.fr>
2022-09-09 08:35:18 +0200

    @deprecated pour les classes test

--- util/src/test/java/fr/ensibs/util/io/TextLoaderImplTest.java
+++ util/src/test/java/fr/ensibs/util/io/TextLoaderImplTest.java
@@ -11,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
  * Unit test for the class that implements the {@link TextLoader} interface
  *
  * @author Pascale Launay
+ * @deprecated
  */
 public class TextLoaderImplTest
 {
