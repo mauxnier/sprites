f28c2ada75f91b26049de51886703c30feeb147a
Bazire Martin <bazire.e1905621@etud.univ-ubs.fr>
2022-09-09 10:04:43 +0200

    Load method + add constructor + add private method

--- util/src/main/java/fr/ensibs/util/io/ZipLoader.java
+++ util/src/main/java/fr/ensibs/util/io/ZipLoader.java
@@ -1,8 +1,11 @@
 package fr.ensibs.util.io;
 
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
@@ -21,26 +24,37 @@ public class ZipLoader implements IZipLoader {
 
     @Override
     public Map<String, Object> load(ZipInputStream in) throws IOException, ParseException {
+        Map<String,Object> res = new HashMap<>();
         ZipInputStream zipIn = new ZipInputStream(in);
         ZipEntry entry;
+        String name, type;
         while ((entry = zipIn.getNextEntry()) != null) {
-            System.out.println(entry.getName());
-            readContents(zipIn);
-            zipIn.closeEntry();
+            name = entry.getName();
+            // Check what is the file type
+            type = getExtensionFromFileName(name);
+            if (type.equals("json"))
+            { // call to Json Loader
+                JSONObject jsonObj = jsonloader.load(zipIn);
+                res.put(name,jsonObj);
+            }else if (type.equals("txt"))
+            { // call to Text Loader
+                String text = textloader.load(zipIn);
+                res.put(name,text);
+            }else{ // in case we can't recognize the format used
+                throw new IOException("ZipLoaderException : unrecognized format");
+            }
+            zipIn.closeEntry(); // close the input stream
         }
-        return null;
+        return res;
     }
 
     @Override
     public void save(Map<String, Object> resources, ZipOutputStream out) throws IOException {
     }
 
-    private void readContents(InputStream contentsIn) throws IOException {
-        byte contents[] = new byte[4096];
-        int direct;
-        while ((direct = contentsIn.read(contents, 0, contents.length)) >= 0) {
-            System.out.println("Read " + direct + "bytes content.");
-        }
+    private String getExtensionFromFileName(String filename)
+    {
+        return filename.substring(filename.indexOf(".")+1);
     }
 
 }
