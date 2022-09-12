c1627cde6f950a4c0de7864a150ad79684ad1af8
Killian Monnier <mauxnier@gmail.com>
2022-09-12 10:25:45 +0200

    imageloader and image

--- util/src/main/java/fr/ensibs/util/io/IImageLoader.java
+++ util/src/main/java/fr/ensibs/util/io/IImageLoader.java
@@ -1,9 +1,10 @@
 package fr.ensibs.util.io;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public interface IImageLoader {
-
-    public void loadImage();
-    public void saveImage();
-
-
+    Image load(InputStream in) throws IOException;
+    void save(Image img, OutputStream out) throws IOException;
 }
---
+++ util/src/main/java/fr/ensibs/util/io/ImageLoader.java
@@ -0,0 +1,19 @@
+package fr.ensibs.util.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.text.ParseException;
+
+public class ImageLoader implements IImageLoader {
+    @Override
+    public Image load(InputStream in) throws IOException {
+        //todo
+        return null;
+    }
+
+    @Override
+    public void save(Image img, OutputStream out) throws IOException {
+        //todo
+    }
+}
