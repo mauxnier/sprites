1b84fec1e5e0e78ab59610beb2a48a9319156151
Bazire Martin <bazire.e1905621@etud.univ-ubs.fr>
2022-09-13 10:57:33 +0200

    modifs

---
+++ sprites.javafx/src/main/java/fr/ensibs/javafx/graphic/JavaFXImage.java
@@ -0,0 +1,18 @@
+package fr.ensibs.javafx.graphic;
+import fr.ensibs.util.graphic.IImage;
+import javafx.scene.image.Image;
+public class JavaFXImage implements IImage {
+
+    private Image img;
+
+    JavaFXImage(Image img)
+    {
+        this.img = img;
+    }
+
+
+    public Image get_image(){
+        return this.img;
+    }
+
+}
---
+++ sprites.javafx/src/main/java/fr/ensibs/javafx/graphic/Main.java
@@ -0,0 +1,47 @@
+package fr.ensibs.javafx.graphic;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+/**
+ * A JavaFX application that displays a directory and is intended to illustrate the MVC architecture
+ *
+ * @author Pascale Launay
+ */
+public class Main extends Application
+{
+    /**
+     * The main entry point for all JavaFX applications.
+     * The start method is called after the init method has returned,
+     * and after the system is ready for the application to begin running.
+     *
+     * <p>
+     * NOTE: This method is called on the JavaFX Application Thread.
+     * </p>
+     *
+     * @param stage the primary stage for this application, onto which
+     *              the application scene can be set.
+     *              Applications may create other stages, if needed, but they will not be
+     *              primary stages.
+     * @throws Exception if something goes wrong
+     */
+    @Override
+    public void start(Stage stage) throws Exception
+    {
+        // init the scene from the FXML description
+        URL resource = Main.class.getResource("main.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(resource);
+        Scene scene = new Scene(fxmlLoader.load());
+
+        // init the stage
+        stage.setScene(scene);
+        stage.setTitle("MVC example");
+
+        // display the frame
+        stage.show();
+    }
+}
