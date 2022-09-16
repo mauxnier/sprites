236827c52390fff572f79a6e5e356acbf90aa817
e2104594 <bakkari.e2104594@etud.univ-ubs.fr>
2022-09-16 12:03:47 +0200

    Add of Snapshots/Layer classes

---
+++ util/src/main/java/fr/ensibs/util/graphic/Snapshot.java
@@ -0,0 +1,13 @@
+package fr.ensibs.util.graphic;
+
+import java.util.List;
+
+/**
+ * A snapshot composed of different images in overlapped layers.
+ *
+ * @param /*<I> the type of images in the snapshot layers
+ * @author Pascale Launay
+ */
+public interface Snapshot/*<I extends Image> extends List<SnapshotLayer<I>>*/
+{
+}
