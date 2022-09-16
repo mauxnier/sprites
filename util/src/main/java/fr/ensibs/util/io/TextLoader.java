05b526200763e9ccbe8064e5166bbd60d24f1a84
Matisse Pedron <matisse@ecobosto.fr>
2022-09-16 12:02:04 +0200

    Change interface name ILoader

--- util/src/main/java/fr/ensibs/util/io/TextLoader.java
+++ util/src/main/java/fr/ensibs/util/io/TextLoader.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class TextLoader implements Loader<String> {
+public class TextLoader implements ILoader<String> {
 
     /**
      * Read a text from an input stream.
