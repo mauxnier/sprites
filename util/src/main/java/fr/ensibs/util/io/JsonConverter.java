440715b45a43661dcb71907e1dcd6a22bfcd5f7f
e2104594 <bakkari.e2104594@etud.univ-ubs.fr>
2022-09-16 09:30:44 +0200

    Add of Interface JsonConverter

--- util/src/main/java/fr/ensibs/util/io/JsonConverter.java
+++ util/src/main/java/fr/ensibs/util/io/JsonConverter.java
@@ -1,4 +1,35 @@
 package fr.ensibs.util.io;
 
-public class JsonConverter {
+import org.json.JSONObject;
+
+import java.text.ParseException;
+
+/**
+ * Interface to convert Java objects to JSON objects and vice versa.
+ *
+ * @param <T> the type of Java objects converted from/to JSON objects
+ * @author Pascale Launay
+ */
+public interface JsonConverter<T>
+{
+    /**
+     * Give a Java object from a JSON object.
+     *
+     * @param obj a JSON object
+     * @return a Java object
+     * @throws ParseException if the given JSON object format doesn't match the generic type
+     * @pre {@code obj != null}
+     * @post {@code result != null}
+     */
+    T fromJson(JSONObject obj) throws ParseException;
+
+    /**
+     * Give a JSON object from a Java object.
+     *
+     * @param obj a Java object
+     * @return a JSON object
+     * @pre {@code obj != null}
+     * @post {@code result != null}
+     */
+    JSONObject toJson(T obj);
 }
