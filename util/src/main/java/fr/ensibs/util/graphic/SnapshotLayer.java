7b313b63e8a8ba9bbe3aecb697a45b90be313baf
Bazire Martin <bazire.e1905621@etud.univ-ubs.fr>
2022-09-16 12:38:08 +0200

    Fill SnapshotLayer

--- util/src/main/java/fr/ensibs/util/graphic/SnapshotLayer.java
+++ util/src/main/java/fr/ensibs/util/graphic/SnapshotLayer.java
@@ -1,4 +1,51 @@
 package fr.ensibs.util.graphic;
 
-public class SnapshotLayer {
+public class SnapshotLayer implements ISnapshotLayer {
+
+    private int x,y;
+
+    private IImage img;
+
+    public SnapshotLayer(int x, int y, IImage img)
+    {
+        this.x = x;
+        this.y = y;
+        this.img = img;
+    }
+
+    @Override
+    public IImage getImage() {
+        return this.img;
+    }
+
+    @Override
+    public int getX() {
+        return this.x;
+    }
+
+    @Override
+    public int getY() {
+        return this.y;
+    }
+
+    @Override
+    public int getWidth() {
+        return img.getWidth();
+    }
+
+    @Override
+    public int getHeight() {
+        return img.getHeight();
+    }
+
+    public void setX(int x)
+    {
+        this.x = x;
+    }
+
+    public void setY(int y)
+    {
+        this.y = y;
+    }
+
 }
