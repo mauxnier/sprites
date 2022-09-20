55b892c4f6a9881e26a98e1da67d713a12abc448
e2104594 <bakkari.e2104594@etud.univ-ubs.fr>
2022-09-20 12:33:34 +0200

    save method JavaFXImageLoader

--- sprites/javafx/src/main/java/fr/ensibs/javafx/graphic/JavaFXImageLoader.java
+++ sprites/javafx/src/main/java/fr/ensibs/javafx/graphic/JavaFXImageLoader.java
@@ -1,12 +1,12 @@
 package fr.ensibs.javafx.graphic;
 
+import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-
-import fr.ensibs.util.graphic.IImage;
 import fr.ensibs.util.io.ILoader;
 import javafx.scene.image.Image;
+import javax.imageio.ImageIO;
 
 public class JavaFXImageLoader implements ILoader<JavaFXImage> {
 
@@ -19,6 +19,8 @@ public class JavaFXImageLoader implements ILoader<JavaFXImage> {
     @Override
     public void save(JavaFXImage img, OutputStream out) throws IOException {
         Image image = img.getImage();
-        out.write(image.getUrl().getBytes()); //TODO je pense que c'est image.getUrl() qu'on veut save mais pas sûr sûr
+        BufferedImage fromFXImage = SwingFXUtils.fromFXImage(image, null);
+        ImageIO.write(fromFXImage, "png", targetImage.toFile());
+        out.write(image.bytes.getBytes());
     }
 }
