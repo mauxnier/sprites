4d427c5d2399a1e7ef53674daa56a131db4e17b1
Killian Monnier <mauxnier@gmail.com>
2022-09-20 11:25:58 +0200

    fix save method in JavaFxImageLoader

--- util/src/main/java/fr/ensibs/util/io/ILoader.java
+++ util/src/main/java/fr/ensibs/util/io/ILoader.java
@@ -1,12 +1,9 @@
 package fr.ensibs.util.io;
 
-
 import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface ILoader<T> {
     public T load(InputStream in) throws Exception;
-
     public void save(T obj, OutputStream out) throws Exception;
-
 }
