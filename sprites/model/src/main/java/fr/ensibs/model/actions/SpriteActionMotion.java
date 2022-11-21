d4e330e1e8b314afd029f36a222f449c89872148
BAZIRE Martin <e1905621>
2022-11-21 12:43:14 +0100

    Update SpriteActionVisibility and motion

---
+++ sprites/model/src/main/java/fr/ensibs/model/actions/SpriteActionMotion.java
@@ -0,0 +1,22 @@
+package fr.ensibs.model.actions;
+
+import fr.ensibs.model.Sprite;
+
+public class SpriteActionMotion extends SpriteAction {
+
+    private final int end;
+
+    public SpriteActionMotion(Sprite sprite, int start, int end) {
+        super(sprite, start);
+        this.end = end;
+    }
+
+    public int getEndTime(){
+       return this.end;
+    }
+
+    @Override
+    public void doAction(int time) {
+
+    }
+}
---
+++ sprites/model/src/main/java/fr/ensibs/model/actions/SpriteActionVisibility.java
@@ -0,0 +1,18 @@
+package fr.ensibs.model.actions;
+
+import fr.ensibs.model.Sprite;
+
+public class SpriteActionVisibility extends SpriteAction {
+
+    private final boolean visible;
+
+    public SpriteActionVisibility(Sprite sprite, int start, boolean visible) {
+        super(sprite, start);
+        this.visible = visible;
+    }
+
+    @Override
+    public void doAction(int time) {
+        this.getSprite().setVisible(visible);
+    }
+}
