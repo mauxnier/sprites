0813821cc1fc87e93ea3d3025f7725453526da2c
Killian Monnier <mauxnier@gmail.com>
2022-11-21 12:24:34 +0100

    spriteAction typage

--- sprites/model/src/main/java/fr/ensibs/model/actions/SpriteAction.java
+++ sprites/model/src/main/java/fr/ensibs/model/actions/SpriteAction.java
@@ -2,17 +2,19 @@ package fr.ensibs.model.actions;
 
 import fr.ensibs.model.ISprite;
 import fr.ensibs.model.Sprite;
+import fr.ensibs.util.graphic.IImage;
+import javafx.scene.image.Image;
 
-public abstract class SpriteAction implements ISpriteAction{
-    private Sprite sprite;
-    private int start;
-    public SpriteAction(Sprite sprite, int start){
+public abstract class SpriteAction<T extends IImage<Image>> implements ISpriteAction<T> {
+    private final Sprite<T> sprite;
+    private final int start;
+    public SpriteAction(Sprite<T> sprite, int start){
         this.sprite = sprite;
         this.start = start;
     }
 
     @Override
-    public ISprite getSprite() {
+    public ISprite<T> getSprite() {
         return this.sprite;
     }
 
