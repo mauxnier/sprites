a0e262db6678966aa48861edb5a15677ac9d958c
BAZIRE Martin <e1905621>
2022-11-21 12:01:29 +0100

    Add SpriteAction class and subclasses

--- sprites/model/src/main/java/fr/ensibs/model/actions/SpriteAction.java
+++ sprites/model/src/main/java/fr/ensibs/model/actions/SpriteAction.java
@@ -1,6 +1,35 @@
+package fr.ensibs.model.actions;
+
+import fr.ensibs.model.ISprite;
+
 public class SpriteAction implements ISpriteAction{
     public SpriteAction(){
         // TO DO
         return;
+    }
+
+    @Override
+    public ISprite getSprite() {
+        return null;
+    }
+
+    @Override
+    public int getStartTime() {
+        return 0;
+    }
+
+    @Override
+    public int getEndTime() {
+        return 0;
+    }
+
+    @Override
+    public void doAction(int time) {
+
+    }
+
+    @Override
+    public boolean isDone() {
+        return false;
     }
 }
