23e3fbffbfcba7b66ff9d7b4b5567849c25e3ac2
BAZIRE Martin <e1905621>
2022-11-28 08:40:17 +0100

    SpriteActionConverter update

--- sprites/model/src/main/java/fr/ensibs/model/actions/SpriteActionConverter.java
+++ sprites/model/src/main/java/fr/ensibs/model/actions/SpriteActionConverter.java
@@ -1,4 +1,24 @@
 package fr.ensibs.model.actions;
 
-public class SpriteActionConverter {
+import fr.ensibs.model.Sprite;
+import fr.ensibs.util.graphic.IImage;
+import fr.ensibs.util.io.IJsonConverter;
+import javafx.scene.image.Image;
+import org.json.JSONObject;
+
+public class SpriteActionConverter<T extends IImage<Image>> implements IJsonConverter<Sprite<T>> {
+
+    public Sprite<T> fromJson(JSONObject obj){
+        /* Visibility or Motion */
+        if(obj.has("visibility")):
+
+        return null;
+    }
+
+    @Override
+    public JSONObject toJson(Sprite<T> obj) {
+
+        return null;
+    }
+
 }
